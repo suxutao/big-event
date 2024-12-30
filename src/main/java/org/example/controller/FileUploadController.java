1da32712814d89a94c803f9ce13ad590f89f2a11
suxutao <113399058+suxutao@users.noreply.github.com>
2024-12-30 14:54:24 +0800

    完成阿里云文件上传功能

---
+++ src/main/java/org/example/controller/FileUploadController.java
@@ -0,0 +1,23 @@
+package org.example.controller;
+
+import org.example.pojo.Result;
+import org.example.utils.AliOssUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) throws Exception {
+        String originalFilename = file.getOriginalFilename();
+        String filename= UUID.randomUUID()+originalFilename.substring(originalFilename.lastIndexOf("."));
+        String url=AliOssUtil.uploadFile(filename,file.getInputStream());
+        return Result.success(url);
+    }
+}
---
+++ src/main/java/org/example/utils/AliOssUtil.java
@@ -0,0 +1,66 @@
+package org.example.utils;
+
+import com.aliyun.oss.*;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.common.comm.SignVersion;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.InputStream;
+
+public class AliOssUtil {
+    private final static String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
+    private final static String REGION = "cn-beijing";
+    private final static String BUCKET_NAME = "big-eventfs";
+
+    public static String uploadFile(String objectName, InputStream in) throws Exception {
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+
+        // 创建OSSClient实例。
+        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
+        OSS ossClient = OSSClientBuilder.create()
+                .endpoint(ENDPOINT)
+                .credentialsProvider(credentialsProvider)
+                .clientConfiguration(clientBuilderConfiguration)
+                .region(REGION)
+                .build();
+
+        String url = "";
+        try {
+            // 填写字符串。
+            String content = "Hello OSS，你好世界";
+
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, objectName, in);
+
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传字符串。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            url = "https://" + BUCKET_NAME + "." + ENDPOINT.substring(ENDPOINT.lastIndexOf('/') + 1) + "/" + objectName;
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return url;
+    }
+}
